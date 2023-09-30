b0cbdc7f58a0321f33dbeb6511e8949325711752
masonarchhsieh <mason.archhsieh@gmail.com>
2023-09-30 21:36:01 +0800

    Time: 16 ms (15.69%), Space: 6.3 MB (14.27%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s = to_string(x);
+        for (auto i = 0; i < s.length() / 2; i++)
+            if (s[i] != s[s.length() - 1 - i])
+                return false;
+        return true;
+    }
+};
