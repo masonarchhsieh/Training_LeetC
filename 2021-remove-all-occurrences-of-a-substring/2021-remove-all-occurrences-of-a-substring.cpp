d8535bb14fbd8cf06f27dbc7e5bb2fb3834ade19
masonarchhsieh <mason.archhsieh@gmail.com>
2023-10-02 11:26:07 +0800

    Time: 5 ms (46.2%), Space: 8.5 MB (10.64%) - LeetHub

---
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        size_t found;
+        while ((found = s.find(part)) != std::string::npos) {
+            s = s.substr(0, found) + s.substr(found + part.length());
+            cout << s << endl;
+        }
+
+        return s;
+    }
+};
