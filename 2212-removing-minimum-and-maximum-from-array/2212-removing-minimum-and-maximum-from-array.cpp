ab28d27a74de71dafb1623fb5952eee1bb3692be
masonarchhsieh <mason.archhsieh@gmail.com>
2023-10-04 18:52:12 +0800

    Time: 129 ms (43.14%), Space: 88.6 MB (22.82%) - LeetHub

--- 2212-removing-minimum-and-maximum-from-array/2212-removing-minimum-and-maximum-from-array.cpp
+++ 2212-removing-minimum-and-maximum-from-array/2212-removing-minimum-and-maximum-from-array.cpp
@@ -3,10 +3,14 @@ public:
     int minimumDeletions(vector<int>& nums) {
         if (nums.size() <= 2)
             return nums.size();
+        int n = nums.size();
+        int min_pos = 0;
+        int max_pos = 0;
         
-        int min_pos = (int) (min_element(nums.begin(), nums.end()) - nums.begin());
-        int max_pos = (int) (max_element(nums.begin(), nums.end()) - nums.begin());
-
+        for (auto i = 0; i < n; i++) {
+            min_pos = (nums[i] < nums[min_pos])? i : min_pos;
+            max_pos = (nums[i] > nums[max_pos])? i : max_pos;
+        }
         int t1 = 1 + min(min_pos, max_pos) + nums.size() - max(min_pos, max_pos);
         int t2 = min(1 + max(min_pos, max_pos), (int) nums.size() - min(min_pos, max_pos));
         return min(t1, t2);
