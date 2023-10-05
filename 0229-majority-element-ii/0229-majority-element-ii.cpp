31ab52eddd6baeb14d8ba1a3f3cd2817706e7d18
masonarchhsieh <mason.archhsieh@gmail.com>
2023-10-05 11:13:42 +0800

    Time: 13 ms (20.8%), Space: 16.1 MB (28.2%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -3,12 +3,16 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         int req = nums.size() / 3;
         vector<int> v;
-        unordered_map<int, int> m;
-        for (auto i : nums)
-            m[i]++;
-        for (auto i : m)
-            if (i.second > req)
-                v.push_back(i.first);
+        sort(nums.begin(), nums.end());
+        int left = 0;
+        for (auto i = 0; i < nums.size(); i++) {
+            if (nums.at(i) != nums.at(left)) {
+                left = i;
+            }
+            // nums.at(left) == nums.at(i)
+            if (i - left == req)
+                v.push_back(nums[left]);
+        }
         return v;
     }
 };
