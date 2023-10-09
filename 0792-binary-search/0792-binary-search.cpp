0011342667b83ff3855b2fa2739249d12af030e1
masonarchhsieh <mason.archhsieh@gmail.com>
2023-10-10 01:13:20 +0800

    Time: 26 ms (72.77%), Space: 28 MB (9.85%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int mid = (right + left) / 2;
+            if (nums.at(mid) == target)
+                return mid;
+            if (nums.at(mid) < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return -1;
+    }
+};
