982bf1724179db7b64108ce39f8b65a09edd624b
masonarchhsieh <mason.archhsieh@gmail.com>
2023-10-10 18:31:58 +0800

    Time: 39 ms (65.14%), Space: 22.1 MB (58.71%) - LeetHub

---
+++ 2713-find-the-divisibility-array-of-a-string/2713-find-the-divisibility-array-of-a-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> divisibilityArray(string word, int m) {
+        vector<int> div(size(word), 0);
+        long long curr_sum = 0;
+
+        for (auto i = 0; i < size(word); i++) {
+            curr_sum = (curr_sum * 10 + word[i] - '0') % m;
+            div[i] = (curr_sum == 0);
+        }
+
+        return div;
+    }
+};
