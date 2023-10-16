62a7008f074350d34f0bdfc6741798c280230e25
masonarchhsieh <mason.archhsieh@gmail.com>
2023-10-16 21:00:34 +0800

    Time: 66 ms (69.15%), Space: 11.8 MB (50.31%) - LeetHub

--- 1530-check-if-a-string-can-break-another-string/1530-check-if-a-string-can-break-another-string.cpp
+++ 1530-check-if-a-string-can-break-another-string/1530-check-if-a-string-can-break-another-string.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
     bool checkIfCanBreak(string s1, string s2) {
-        multiset<char> s1_set, s2_set;
-        for (auto i = 0; i < size(s1); i++) {
-            s1_set.insert(s1[i]);
-            s2_set.insert(s2[i]);
-        }
+        if (size(s1) != size(s2))
+            return false;
+        sort(s1.begin(), s1.end());
+        sort(s2.begin(), s2.end());
 
-        int mode = -1;
-        for (multiset<char>::iterator it_s1 = s1_set.begin(), it_s2 = s2_set.begin(); it_s1 != s1_set.end() && it_s2 != s2_set.end(); ++it_s1, ++it_s2) {
-            if (mode < 0) {
-                if (*it_s1 < *it_s2)
-                    mode = 1;
-                else if (*it_s1 > *it_s2)
-                    mode = 2;
-            }
-            
-            if (mode == 1) {
-                if (*it_s1 > *it_s2)
-                    return false;
-            }
-            else if (mode == 2) {
-                if (*it_s1 < *it_s2)
-                    return false;
-            }
-        }
+        return helper(s1, s2) | helper(s2, s1);
+    }
 
-        return true;;
+    bool helper(const string& s1, const string& s2) {
+        for (size_t i = 0; i < size(s1); i++)
+            if (s1[i] < s2[i])
+                return false;
+        return true;
     }
 };
