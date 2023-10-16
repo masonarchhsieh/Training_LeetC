f71a1fe5aace5562234d4096451fd322c17f588b
masonarchhsieh <mason.archhsieh@gmail.com>
2023-10-16 13:49:53 +0800

    Time: 2 ms (60.95%), Space: 7.2 MB (37.22%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<bool> inStack(std::size(s), false);
+        vector<int> counts(26, 0);
+        stack<char> st;
+
+        for (const char c : s)
+            counts[c - 'a']++;
+        
+        for (const char c : s) {
+            counts[c - 'a']--;
+
+            if (inStack[c - 'a'])
+                continue;
+            while (!st.empty() && c < st.top() && counts[st.top() - 'a'] > 0) {
+                inStack[st.top() - 'a'] = false;
+                st.pop();
+            }
+            st.push(c);
+            inStack[c - 'a'] = true;
+        }
+
+        string res;
+        while (!st.empty()) {
+            res = st.top() + res;
+            st.pop();
+        }
+
+        return res;
+    }
+};
