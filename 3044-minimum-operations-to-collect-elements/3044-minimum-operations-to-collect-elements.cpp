025a656d491b8b62fff01521295fc88d8499b657
masonarchhsieh <mason.archhsieh@gmail.com>
2023-10-16 11:35:22 +0800

    Time: 8 ms (31.32%), Space: 19.3 MB (24.03%) - LeetHub

--- 3044-minimum-operations-to-collect-elements/3044-minimum-operations-to-collect-elements.cpp
+++ 3044-minimum-operations-to-collect-elements/3044-minimum-operations-to-collect-elements.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        vector<bool> collected(k, false);
+        unordered_set<int> _set;
         int ops = 0;
         for (int i = size(nums) - 1; i >= 0; i--) {
-            if (nums.at(i) - 1 < k)
-                collected[nums.at(i) - 1] = true;
+            if (nums.at(i) <= k)
+                _set.insert(nums.at(i));
             ops++;
-            if (std::find(collected.begin(), collected.end(), false) == collected.end())
+            if (size(_set) == k)
                 break;
         }
         return ops;
