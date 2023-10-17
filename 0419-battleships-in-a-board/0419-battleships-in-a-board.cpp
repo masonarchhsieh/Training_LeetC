1ed7ac2e13d6e17a0307bc5c930f7b097d93913b
masonarchhsieh <mason.archhsieh@gmail.com>
2023-10-17 12:16:27 +0800

    Time: 3 ms (95.15%), Space: 8.8 MB (13.31%) - LeetHub

---
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int countBattleships(vector<vector<char>>& board) {
+        int cnt = 0;
+        int n = board.size(), m = board[0].size();
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
+
+        for (auto i = 0; i < n; i++) {
+            for (auto j = 0; j < m; j++) {
+                if (board[i][j] == 'X' && !visited[i][j]) {
+                    helper(board, visited, i, j, -1);
+                    cnt++;
+                }
+            }
+        }
+        
+
+        return cnt;
+    }
+
+    void helper(vector<vector<char>>& board, vector<vector<bool>>& visited, int i, int j, int mode) {
+        if (i < 0 || i >= board.size())
+            return;
+        if (j < 0 || j >= board[i].size())
+            return;
+        if (board[i][j] != 'X')
+            return;
+        if (visited[i][j])
+            return;
+        visited[i][j] = true;
+        // horizontal
+        if (mode == -1 || mode == 0)
+            helper(board, visited, i - 1, j, 0);
+        if (mode == -1 || mode == 1)
+            helper(board, visited, i + 1, j, 1);
+        if (mode == -1 || mode == 2)
+            helper(board, visited, i, j - 1, 2);
+        if (mode == -1 || mode == 3)
+            helper(board, visited, i, j + 1, 3);
+    }
+};
