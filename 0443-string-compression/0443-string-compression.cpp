ad38b343b0cfd30c299df03066cdfdcb34b1b28d
masonarchhsieh <mason.archhsieh@gmail.com>
2023-10-23 13:30:01 +0800

    Time: 7 ms (28.43%), Space: 9.4 MB (23.76%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -6,18 +6,17 @@ public:
         auto index = 0;
         for (l = 0, r = 0; r < size(chars);) {
             l = r;
-            while (r < size(chars) && chars.at(r) == chars.at(l)) {
+            while (r < size(chars) && chars.at(r) == chars.at(l))
                 r++;
-            }
+
             res.push_back(chars.at(l));
             if (r - l > 1)
                 res += to_string(r - l);
         }
 
         chars.resize(size(res));
-        for (auto i = 0; i < size(res); i++) {
+        for (auto i = 0; i < size(res); i++) 
             chars[i] = res[i];
-        }
 
         return size(chars);
     }
