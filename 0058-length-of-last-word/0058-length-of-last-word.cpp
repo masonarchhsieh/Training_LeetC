581af4d28759f127f495666ce174a1bbf29fe5e3
masonarc <mason.archhsieh@gmail.com>
2024-04-01 21:18:47 +0800

    Time: 5 ms (47.87%), Space: 6.5 MB (100%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int prevLen = 0, curLen = 0;
-
-        for (auto i = 0; i < s.length(); i++) {
-            if (isspace(s[i])) {
-                curLen = 0;
-                continue;
-            }
-            curLen++;
-            prevLen = (curLen == 0)? prevLen : curLen;
+        int itr = s.length() - 1;
+        int res = 0;
+        while (isspace(s[itr])) {
+            itr--;
         }
-
-        return prevLen;
+        
+        while(itr >= 0 && isalpha(s[itr])) {
+            res++;
+            itr--;
+        }
+         
+        return res;
     }
 };
