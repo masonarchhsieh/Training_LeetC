57c8c5be6746752c7952465410befb14a24bd667
masonarc <mason.archhsieh@gmail.com>
2024-04-02 22:33:30 +0800

    Time: 3 ms (91.66%), Space: 9.4 MB (17.52%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,26 +1,21 @@
 class Solution {
- 
-    
 public:
     bool isIsomorphic(string s, string t) {
-        std::unordered_map<char,char> hash_map;
-        int mapped[257] = {0};
-        
-        int len = s.length();
-        for (int i=0; i<len; i++)
-        {
-            if (hash_map.count(s[i])) {
-                if (hash_map[s[i]] != t[i])
-                    return false;
-            } else {
-                if (!mapped[t[i]]) {
-                    hash_map[s[i]] = t[i];
-                    mapped[t[i]] = true;
-                }
-                else
+        unordered_map<char, char> m;
+
+        for (auto i = 0; i < s.length(); i++) {
+            if (m.find(s[i]) != m.end()) {
+                if (m[s[i]] == t[i])
+                    continue;
+                return false;
+            }
+            for (auto c : m) {
+                if (c.second == t[i])
                     return false;
             }
+            m[s[i]] = t[i];
         }
+
         return true;
     }
 };
