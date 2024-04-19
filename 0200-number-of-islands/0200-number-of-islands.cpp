06ac3f91b6f2422878c8bb5cccce4d4a5bbb86ef
masonarc <mason.archhsieh@gmail.com>
2024-04-19 20:54:18 +0800

    Time: 28 ms (62.85%), Space: 15.7 MB (81.63%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,34 +1,28 @@
 class Solution {
-  
-    
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int res = 0;
-        int m = grid.size();
-        int n = grid[0].size();
-        
-        for(int i=0;i<m; i++) {
-            for (int j=0;j<n; j++) {
-                if (grid[i][j] =='1'){
+        int islands = 0; 
+        for (auto i = 0; i < grid.size(); i++) {
+            for (auto j = 0; j < grid[i].size(); j++) {
+                if (grid[i][j] == '1') {
                     dfs(grid, i, j);
-                    res++;
+                    islands++;
                 }
             }
         }
-        return res;
+
+        return islands;
     }
-    
-    void dfs(vector<vector<char>>& grid, int i, int j)
-    {
+
+    void dfs(vector<vector<char>>& grid, int i, int j) {
         grid[i][j] = '0';
-        
-        if (i+1<grid.size() && grid[i+1][j] == '1')
-            dfs(grid, i+1, j);
-        if (j+1<grid[0].size() && grid[i][j+1] == '1')
-            dfs(grid, i, j+1);
-        if (i-1>=0 && grid[i-1][j] == '1')
-            dfs(grid, i-1, j);
-        if (j-1>=0 && grid[i][j-1] == '1')
-            dfs(grid, i, j-1);
+        if (i + 1 < grid.size() && grid[i + 1][j] == '1')
+            dfs(grid, i + 1, j);
+        if (j + 1 < grid[i].size() && grid[i][j + 1] == '1')
+            dfs(grid, i, j + 1);
+        if (i - 1 >= 0 && grid[i - 1][j] == '1')
+            dfs(grid, i - 1, j);
+        if (j - 1>= 0 && grid[i][j - 1] == '1')
+            dfs(grid, i, j - 1);
     }
 };
