b327338d6b8edbc6f7b0a0cf3a58f2130b575ea1
masonarc <mason.archhsieh@gmail.com>
2024-04-24 22:08:47 +0800

    Time: 0 ms (100%), Space: 6.3 MB (100%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     int tribonacci(int n) {
-        vector<int> tribonacci_(38, 0);
-        tribonacci_[1] = 1;
-        tribonacci_[2] = 1;
-
-        for (auto i = 3; i <= n; i++) {
-            tribonacci_[i] = tribonacci_[i-1] + tribonacci_[i-2] + tribonacci_[i-3];
-        }
-
-        return tribonacci_[n];
+        if (n == 0)
+            return 0;
+        else if (n <= 2)
+            return 1;
+        
+        int f0 = 0, f1 = 1, f2 = 1;
+        int res = 0;
+        while (n-- > 2) {
+            res = f0 + f1 + f2;
+            f0 = f1;
+            f1 = f2;
+            f2 = res;           
+        } 
+        
+        
+        return res;
     }
 };
