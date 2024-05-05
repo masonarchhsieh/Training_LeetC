6dff4565d244f5200e69db24a4ef367c9815190c
masonarc <mason.archhsieh@gmail.com>
2024-05-05 21:35:07 +0800

    Time: 6 ms (53.82%), Space: 11.4 MB (5.44%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -9,27 +9,6 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        if (!node->next) {
-            node = NULL;
-            return;
-        }
-
-        ListNode *itr = node, *nxt = node->next;
-        while (itr) {
-            while (nxt && nxt->val == node->val) {
-                nxt = nxt->next;
-            }
-            if (!nxt) {
-                itr->next = NULL;
-                break;
-            }
-            itr->val = nxt->val;
-            nxt = nxt->next;
-            if (!nxt) {
-                break;
-            }
-            itr = itr->next;
-        }
-        itr->next = NULL;
+        *node = *node->next;
     }
 };
