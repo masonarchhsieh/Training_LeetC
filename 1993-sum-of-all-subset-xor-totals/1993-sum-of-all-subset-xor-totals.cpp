ccc3ddfc3e954a30c178b74b8ba89644c4767354
masonarc <mason.archhsieh@gmail.com>
2024-05-20 22:33:25 +0800

    Time: 4 ms (55.14%), Space: 7.2 MB (100%) - LeetHub

---
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int subsetXORSum(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        dfs(0, nums, 0, ans, n);
+        return ans;
+    }
+
+    void dfs(int idx, vector<int>& nums, int sums, int &ans, int n) {
+        if (idx == n) {
+            ans += sums;
+            return;
+        }
+        // take
+        dfs(idx + 1, nums, sums ^ nums[idx], ans, n);
+        // not take
+        dfs(idx + 1, nums, sums, ans, n);
+    }
+};
