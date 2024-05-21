83550f3fc622af555ba0ca7f45c52d6e3bfc9020
masonarc <mason.archhsieh@gmail.com>
2024-05-21 22:41:58 +0800

    Time: 5 ms (14.01%), Space: 7.2 MB (100%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,20 +1,25 @@
 class Solution {
-    vector<vector<int>> res;
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        helper(nums, {}, 0);
+        vector<vector<int>> res;
+        vector<int> ds;
+        
+        helper(0, nums, ds, res);
         return res;
     }
-
-    void helper(vector<int>& nums, vector<int> v, int i) {
-        if (i >= nums.size()) {
-            res.push_back(v);
+    
+    void helper(int idx, vector<int>& nums, vector<int>& ds, vector<vector<int>>& res)
+    {
+        if (idx == nums.size())
+        {
+            res.push_back(ds);
             return;
         }
-        v.push_back(nums.at(i));
-        helper(nums, v, i + 1);
-        v.pop_back();
-        helper(nums, v, i + 1);
+        
+        ds.push_back(nums[idx]);
+        helper(idx + 1, nums, ds, res);
+        
+        ds.pop_back();
+        helper(idx + 1, nums, ds, res);
     }
-
 };
