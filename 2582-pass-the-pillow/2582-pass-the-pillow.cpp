8c4edecf0b56acf67845913fd0d3d58464fb494d
masonarc <mason.archhsieh@gmail.com>
2024-07-06 16:28:47 +0800

    Time: 0 ms (100%), Space: 6.4 MB (100%) - LeetHub

---
+++ 2582-pass-the-pillow/2582-pass-the-pillow.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        int itr = 1, dir = 1;
+
+        while (time-- > 0) {
+            if (itr == n) {
+                dir = -1;
+            }
+            else if (itr == 1) {
+                dir = 1;
+            }
+            itr += dir * 1;
+        }
+        return itr;
+    }
+};
