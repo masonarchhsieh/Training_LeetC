0fb70be28d43bba4435bf215c4c11ef487779d33
masonarc <mason.archhsieh@gmail.com>
2024-07-24 00:34:42 +0800

    Time: 3 ms (89.91%), Space: 15.3 MB (5.99%) - LeetHub

--- 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cpp
@@ -1,42 +1,30 @@
-bool cmp(const pair<int, int> &T1, const pair<int, int> &T2) {
-    if (T1.second == T2.second)
-        return T1.first > T2.first;
-    return T1.second < T2.second;
-}
-
+struct Compare {
+    bool operator()(const pair<int, int> &T1, const pair<int, int> &T2) {
+        if (T1.second == T2.second)
+            return T1.first < T2.first;
+        return T1.second >= T2.second;
+    }
+};
 class Solution {
+
 public:
     vector<int> frequencySort(vector<int>& nums) {
-        if (nums.size() == 1)
-            return nums;
-        
+        unordered_map<int, int> m;
         vector<int> res;
-        vector<pair<int, int>> nums_list;
-        
-        sort(nums.begin(), nums.end());
-        int cnt = 1;
-        for (auto i = 1; i < nums.size(); i++) {
-            if (i == nums.size() - 1) {
-                if (nums[i] != nums[i - 1]) {
-                    nums_list.push_back(make_pair(nums[i - 1], cnt));
-                    nums_list.push_back(make_pair(nums[i], 1));
-                }
-                else
-                    nums_list.push_back(make_pair(nums[i], cnt + 1));
-                break;
-            }
-            else if (nums[i] != nums[i - 1]) {
-                nums_list.push_back(make_pair(nums[i - 1], cnt));
-                cnt = 1;
-            } else
-                cnt++;
-        }
-        sort(nums_list.begin(), nums_list.end(), cmp);
-        for (auto i : nums_list)
-            for (auto j = 0; j < i.second; j++) {
-                res.push_back(i.first);
-            }  
+        for (auto i : nums)
+            m[i]++;
 
+        priority_queue<pair<int, int>, vector<pair<int,int>>, Compare> pq;
+        for (auto i : m) {
+            pq.push({i.first, i.second});
+        }
+
+        while (!pq.empty()) {
+            for (auto i = 0; i < pq.top().second; i++) {
+                res.push_back(pq.top().first);
+            }
+            pq.pop();
+        }
         return res;
     }
 };
