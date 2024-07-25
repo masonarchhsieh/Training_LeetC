e98440f39c882fa9419fd917dd832638aae5cc61
masonarc <mason.archhsieh@gmail.com>
2024-07-25 15:54:16 +0800

    Time: 742 ms (13.95%), Space: 102.1 MB (33.07%) - LeetHub

--- 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
@@ -1,39 +1,23 @@
 class Solution {
 public:
     vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
-        vector<int> v;
+        auto jumble_int = [&](int num) -> int {
+            string digits = to_string(num);
+            for (char &digit : digits) {
+                digit = '0' + mapping[digit - '0'];
+            }
+            return stoi(digits);
+        };
+
         unordered_map<int, int> m;
-        bool same = false;
-        
-        for (auto i = 0; i < nums.size(); i++) {
-            int num = 0, dec = 1;
-            int n = nums.at(i);
-            while (n > 0) {
-                num = dec * (mapping.at(n % 10)) + num;
-                n /= 10;
-                dec *= 10;
+        for (auto num : nums) {
+            if (m.find(num) == m.end()) {
+                m[num] = jumble_int(num);
             }
-            v.push_back(num);
-            same |= (m[num]++ >= 1);
         }
-        
-        if (!same) {
-            sort(begin(v), end(v));
-            return v;
-        }
-
-
-        vector<int> v1, v2;
-        for (auto i = 0; i < v.size(); i++) {
-            if (m[v[i]] == 1) {
-                v2.push_back(nums[i]);
-                continue;
-            }
-            v1.push_back(nums[i]);
-        }
-        sort(begin(v2), end(v2));
-        v1.insert(end(v1), begin(v2), end(v2));
-
-        return v1;
+        sort(begin(nums), end(nums), [&](int a, int b) {
+            return m[a] < m[b];
+        });
+        return nums;
     }
 };
