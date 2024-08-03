7ae29ad82cb59f85fb6a9a14a553a37672330039
masonarc <mason.archhsieh@gmail.com>
2024-08-03 22:04:59 +0800

    Time: 8 ms (48.95%), Space: 13.6 MB (100%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     int countSeniors(vector<string>& details) {
-        return std::count_if(begin(details), end(details), [](auto detail){ return detail[11] > '6' || (detail[11] == '6' && detail[12] > '0'); });
+        int cnt = 0;
+
+        for (auto i : details) {
+            if (stoi(i.substr(11, 2)) > 60)
+                cnt++;
+        }
+
+        return cnt;
     }
 };
